63f6b40c9513fd557915396312bc3091dcf3232d
nirajgeorgian <nirajgeorgian01@gmail.com>
2016-12-14 16:47:42 +0530

    finished making a simple file

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database:'mongodb://localhost/amazon',
+  database:'mongodb://root:pass@ds133418.mlab.com:33418/forum_demo',
   port: process.env.PORT || 3000,
   secretKey: "ninnijs@N9#" // Used for session on server // Cookies on client
 }
